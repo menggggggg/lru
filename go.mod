0377bdd5266f083c3e753af3331d7c0a4342d031
zhaomeng <zm209516@alibaba-inc.com>
2019-08-28 22:55:57 +0800

    feat:添加readme

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/menggggggg/lru
+
+go 1.12
+
+require (
+	github.com/stretchr/testify v1.4.0
+	golang.org/x/crypto v0.0.0-20190820162420-60c769a6c586 // indirect
+	golang.org/x/lint v0.0.0-20190409202823-959b441ac422 // indirect
+	golang.org/x/net v0.0.0-20190827160401-ba9fcec4b297 // indirect
+	golang.org/x/sys v0.0.0-20190826190057-c7b8b68b1456 // indirect
+	golang.org/x/text v0.3.2 // indirect
+	golang.org/x/tools v0.0.0-20190827205025-b29f5f60c37a // indirect
+)
--- lru.go
+++ lru.go
@@ -4,66 +4,66 @@ import "fmt"
 
 type LRUCache struct {
 	capacity int
-	maps map[string]*Node
-	head *Node
-	tail *Node
+	maps     map[string]*Node
+	head     *Node
+	tail     *Node
 }
 
-func NewLRUCache(cap int) *LRUCache{
-	if cap == 0{
+func NewLRUCache(cap int) *LRUCache {
+	if cap == 0 {
 		cap = 1024
 	}
 
 	lru := &LRUCache{
-		capacity:cap,
-		maps:make(map[string]*Node),
-		head:new(Node),
-		tail:new(Node),
+		capacity: cap,
+		maps:     make(map[string]*Node),
+		head:     new(Node),
+		tail:     new(Node),
 	}
 	lru.head.next = lru.tail
 	lru.tail.pre = lru.head
 	return lru
 }
 
-func (c *LRUCache)Get(key string) (value interface{},exist bool){
-	if node,ok := c.maps[key];ok{
+func (c *LRUCache) Get(key string) (value interface{}, exist bool) {
+	if node, ok := c.maps[key]; ok {
 		c.moveTop(node)
-		return node.value,true
+		return node.value, true
 	}
-	return nil,false
+	return nil, false
 }
 
-func (c *LRUCache)Put(key string,value string){
-	node,ok := c.maps[key]
+func (c *LRUCache) Put(key string, value string) {
+	node, ok := c.maps[key]
 	if ok {
 		node.value = value
 		c.moveTop(node)
 		return
 	}
-	if len(c.maps) >= c.capacity{
+	if len(c.maps) >= c.capacity {
 		c.moveTail()
 	}
-	node = NewNode(key,value)
+	node = NewNode(key, value)
 	c.addTop(node)
 }
 
-func (c *LRUCache)PreList(){
-	for node := c.head.next;node.next!= nil;node = node.next {
-		fmt.Println(node.key," ",node.value)
+func (c *LRUCache) PreList() {
+	for node := c.head.next; node.next != nil; node = node.next {
+		fmt.Println(node.key, " ", node.value)
 	}
 }
 
-func (c *LRUCache)LastList(){
-	for node := c.tail.pre;node.pre!= nil;node = node.pre {
-		fmt.Println(node.key," ",node.value)
+func (c *LRUCache) LastList() {
+	for node := c.tail.pre; node.pre != nil; node = node.pre {
+		fmt.Println(node.key, " ", node.value)
 	}
 }
 
-func (c *LRUCache)Len()int{
+func (c *LRUCache) Len() int {
 	return len(c.maps)
 }
 
-func (c *LRUCache)moveTop(node *Node){
+func (c *LRUCache) moveTop(node *Node) {
 	node.pre.next = node.next
 	node.next.pre = node.pre
 
@@ -74,14 +74,14 @@ func (c *LRUCache)moveTop(node *Node){
 	c.head.next = node
 }
 
-func (c *LRUCache) moveTail(){
+func (c *LRUCache) moveTail() {
 	fmt.Println(c.tail.pre.key)
-	delete(c.maps,c.tail.pre.key)
-	c.tail.pre.pre.next  = c.tail
+	delete(c.maps, c.tail.pre.key)
+	c.tail.pre.pre.next = c.tail
 	c.tail.pre = c.tail.pre.pre
 }
 
-func (c *LRUCache)addTop(node *Node){
+func (c *LRUCache) addTop(node *Node) {
 	c.maps[node.key] = node
 	node.next = c.head.next
 	node.pre = c.head
